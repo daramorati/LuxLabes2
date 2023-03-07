134af26b1b03bbc2819529d45689ef88f4a7cd11
AlessandraNascimentoB <alessandra3632@gmail.com>
2023-03-07 14:19:42 -0300

    ajeitando o menu

--- assets/index.js
+++
@@ -1,8 +0,0 @@
-const botaoMenu = document.querySelector('.cabecalho__menu')
-const menu = document.querySelector('.menu__lateral')
-
-const link = document.querySelector('.menu__lateral-link')
-
-botaoMenu.addEventListener('click', () => {
-    menu.classList.toggle('menu__lateral--ativo')
-})
--- assets/script.js
+++ assets/script.js
@@ -5,7 +5,7 @@ function toggleMenu(event) {
   const nav = document.getElementById('nav');
   nav.classList.toggle('active');
   const active = nav.classList.contains('active');
-  event.currentTarget.setAttribute('aria-expanded', active);
+    event.currentTarget.setAttribute('aria-expanded', active);
   if (active) {
     event.currentTarget.setAttribute('aria-label', 'Fechar Menu');
   } else {
